a529273e009ec48ef6221561702963d3dd1f4f6b
C0A23112 <c0a231125c@edu.teu.ac.jp>
2024-07-02 17:52:25 +0900

    追加機能2修正

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -301,7 +301,7 @@ def main():
 
         for emy in pg.sprite.groupcollide(emys, beams, True, True).keys():
             exps.add(Explosion(emy, 100))  # 爆発エフェクト
-            score.value += 1000  # 10点アップ
+            score.value += 10  # 10点アップ
             bird.change_img(6, screen)  # こうかとん喜びエフェクト
 
         for bomb in pg.sprite.groupcollide(bombs, beams, True, True).keys():
